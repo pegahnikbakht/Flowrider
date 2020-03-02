18858658736980e7b7f3609c59ddbb16bedff9c6
Nicolae <nicolae.paladi@eit.lth.se>
2020-03-02 15:10:07 +0000

    Add testbed scaffolding

---
+++ Testbed/README.txt
@@ -0,0 +1,4 @@
+Installing based on: https://ewen.mcneill.gen.nz/blog/entry/2014-10-07-ryu-and-openvswitch-on-docker/
+
+
+
---
+++ Testbed/cleanup.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+docker stop flowrider_h1
+docker stop flowrider_h2
+
+docker rm flowrider_h1
+docker rm flowrider_h2
+
+
